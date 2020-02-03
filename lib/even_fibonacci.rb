13dcaf3efb0869ffa8458c1f495bc21cec1de8b3
mattbechtel1 <bechma02@gmail.com>
2020-02-03 09:26:52 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+
+def fibonacci_builder(limit)
+    numArray = [1, 2]
+    while numArray[-1] + numArray[-2] <= limit
+        numArray.push(numArray[-1] + numArray[-2])
+    end
+    numArray
+end
+
+def even_fibonacci_sum(limit)
+    fibonacci_builder(limit).reduce(0) {|memo, num| num % 2 == 0 ? memo + num : memo }
+end
